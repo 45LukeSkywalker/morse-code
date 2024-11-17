cb692bf821bba00ab2ca457590d2d0837d091ca0
45LukeSkywalker <153367315+45LukeSkywalker@users.noreply.github.com>
2024-11-17 10:33:28 +0000

    Started making transliteration hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut morse_table: HashMap<&str, char> = HashMap::new();
+    morse_table.insert(".-", 'A');
+    morse_table.insert("-...", 'B');
+    morse_table.insert("-.-.", 'C');
+    morse_table.insert("-..", 'D');
 }
